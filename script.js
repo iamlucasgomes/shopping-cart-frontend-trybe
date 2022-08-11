2de15482880b72cc80070bafcf7db5b586714679
iamlucasgomes <lucas.devjs@gmail.com>
2022-08-11 13:24:38 -0300

    UPDATE: requeriment 05 implemented

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ const createProductItemElement = ({ sku, name, image }) => {
 const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
 
 const cartItemClickListener = (event) => {
-  console.log(event.target);
+  event.path[0].remove();
 };
 
 const createCartItemElement = ({ sku, name, salePrice }) => {
